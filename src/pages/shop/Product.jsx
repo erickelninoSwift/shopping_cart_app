36c82eee61d913fa31d4ab54de717b3d778fbadc
Erick El nino <erick.tshimbombo@yahoo.com>
2023-12-26 02:38:05 +0200

    creatting the products page

--- src/pages/shop/Product.jsx
+++ src/pages/shop/Product.jsx
@@ -1,8 +1,18 @@
 import React from "react";
-
+import "./shop.css";
 const Product = ({ productsInfo }) => {
   const { id, productName, price, productImage } = productsInfo;
-  return <div>{productName}</div>;
+  return (
+    <div className="product">
+      <img src={productImage} alt={productName} height={"150px"} width="auto" />
+      <div className="description">
+        <p>
+          <b>{productName}</b>
+        </p>
+        <p>R{price}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
